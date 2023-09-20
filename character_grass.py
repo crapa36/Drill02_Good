06674effd11bffef932195a55bbece479e1073e6
Crapa-Laptop <crapa36@gmail.com>
2023-09-20 17:53:56 +0900

    add draw character and grass

--- character_grass.py
+++ character_grass.py
@@ -5,7 +5,12 @@ open_canvas()
 grass = load_image('grass.png')
 character=load_image('character.png')
 def runCircle():
-    pass
+    clear_canvas_now()
+    grass.draw_now(800/2,30)
+    character.draw_now(400,90)
+    delay(1)
+    
+
 def runRectangle():
     pass
 
