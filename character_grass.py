d4b0e527c2341b221875f6a3893a80fbde3ee840
Crapa-Laptop <crapa36@gmail.com>
2023-09-20 17:44:28 +0900

    Update character_grass.py

--- character_grass.py
+++ character_grass.py
@@ -4,3 +4,14 @@ open_canvas()
 
 grass = load_image('grass.png')
 character=load_image('character.png')
+def runCircle():
+    pass
+def runRectangle():
+    pass
+
+while(1):
+    runCircle
+    runRectangle
+    break
+
+close_canvas
