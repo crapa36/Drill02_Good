d450dcf2054ba3020657b7ced224a0bfcf4602e1
Crapa-Laptop <crapa36@gmail.com>
2023-09-20 18:12:30 +0900

    add renderAll()

--- character_grass.py
+++ character_grass.py
@@ -6,23 +6,24 @@ grass = load_image('grass.png')
 character=load_image('character.png')
 
 delay(1)
+
+def renderAll(x,y):
+    clear_canvas_now()
+    grass.draw_now(800/2,30)
+    character.draw_now(x, y)
+    delay(0.01)
+
 def runCircle():
     cx, cy, r = 400,300,200
     for degree in range(0,360,5):
         x=cx+r*math.cos(math.radians(degree))
         y=cy+r*math.sin(math.radians(degree))
-        clear_canvas_now()
-        grass.draw_now(800/2,30)
-        character.draw_now(x, y)
-        delay(0.05)
+        renderAll(x,y)
 
 def runRectangle():
     for x in range(50, 750, 5):
        y=90
-       clear_canvas_now()
-       grass.draw_now(800/2,30)
-       character.draw_now(x, y)
-       delay(0.01)
+       renderAll(x,y)
 
 while True:
     runCircle()
